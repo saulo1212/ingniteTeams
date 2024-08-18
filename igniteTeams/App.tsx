49da52d4a73385fea1e381303d91e0a2d6f221bc
saulo1212 <saulodetacio715@gmail.com>
2024-08-18 18:12:53 -0300

    Loading component adn  styling the statusBar

--- igniteTeams/App.tsx
+++ igniteTeams/App.tsx
@@ -1,18 +1,28 @@
 import Groups from '@screens/Groups';
 import {ThemeProvider} from 'styled-components'
 import theme from './src/themes/'
-import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
-import { ActivityIndicator } from 'react-native';
+import {
+  useFonts, 
+  Roboto_400Regular, 
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
 
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
-
-
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular, Roboto_700Bold
+  })
 
   return (
    <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator/> }
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
+      { fontsLoaded ? <Groups /> : <Loading/> }
    </ThemeProvider>
   );
 }
