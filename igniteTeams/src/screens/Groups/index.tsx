487d5bbbdc516e9a219ac96c3e7cc78293b33d5b
saulo1212 <saulodetacio715@gmail.com>
2024-08-19 13:29:12 -0300

    Component header

--- igniteTeams/src/screens/Groups/index.tsx
+++ igniteTeams/src/screens/Groups/index.tsx
@@ -1,11 +1,13 @@
 
 
+import { Header } from '@components/Header';
 import {Container} from './styles';
 
 export default function Groups() {
 
   return (
     <Container >
+      <Header />
     </Container>
   );
 }
