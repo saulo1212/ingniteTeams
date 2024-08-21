f73c3964a6f8006f8dffe6e5e41f79fbb914e8cd
saulo1212 <saulodetacio715@gmail.com>
2024-08-21 09:26:01 -0300

    Creating Feature Component

--- igniteTeams/src/screens/Groups/index.tsx
+++ igniteTeams/src/screens/Groups/index.tsx
@@ -1,11 +1,16 @@
-import { Header } from '@components/Header';
-import {Container} from './styles';
+import { Header } from '@components/Header'
+import {Highlight} from '@components/Highlight'
+import {Container} from './styles'
 
 export default function Groups() {
 
   return (
     <Container >
       <Header  />
+      <Highlight 
+        title='Turmas'
+        subtitle='Jogue com a sua turma'
+      />
     </Container>
   );
 }
