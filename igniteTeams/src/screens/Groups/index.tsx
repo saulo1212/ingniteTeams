ab34b103c1b86ce3b049a358ec1c02571cb7cb3a
saulo1212 <saulodetacio715@gmail.com>
2024-08-27 21:33:43 -0300

    List Empty

--- igniteTeams/src/screens/Groups/index.tsx
+++ igniteTeams/src/screens/Groups/index.tsx
@@ -2,17 +2,40 @@ import { Header } from '@components/Header'
 import {Highlight} from '@components/Highlight'
 import {GroupCard} from '@components/GroupCard'
 import {Container} from './styles'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+import { ListEmpty } from '@components/ListEmpty'
 
 export default function Groups() {
 
+  const [groups,setGroups] = useState<string[]>(
+    [
+      'Grupo de Teste', 
+      'Grupo de teste 2'
+    ]
+)
+
   return (
     <Container >
+
       <Header  />
+
       <Highlight 
         title='Turmas'
         subtitle='Jogue com a sua turma'
       />
-      <GroupCard  title='Galera do ingnite' />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <GroupCard title={item}/>
+        )}
+
+        contentContainerStyle={groups.length === 0 && {flex:1}}
+        ListEmptyComponent={() => <ListEmpty message='Cadastre uma turma' />}
+      />
+
     </Container>
   );
 }
