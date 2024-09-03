b23faeb54c0f89ed1fb8bb77e13b1851698f22cd
saulo1212 <saulodetacio715@gmail.com>
2024-09-03 19:40:45 -0300

    Creating button icon component

--- igniteTeams/src/screens/Players/index.tsx
+++ igniteTeams/src/screens/Players/index.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@components/Header'
 import {Container} from './styles'
 import { Highlight } from '@components/Highlight'
+import { ButtonIcon } from '@components/ButtonIcon'
 
 export function Players(){
     return(
         <Container>
             <Header showBackButton/>
             <Highlight title='Nome da turma' subtitle='Adicione e separe os times' />
+            <ButtonIcon />
         </Container>
     )
 }
