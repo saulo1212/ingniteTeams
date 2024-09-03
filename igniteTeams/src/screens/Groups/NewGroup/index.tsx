6c97a3e0c32e5b85a03bbf5ac5a75301cafbeb80
saulo1212 <saulodetacio715@gmail.com>
2024-09-02 21:57:13 -0300

    css helper refactor

--- igniteTeams/src/screens/Groups/NewGroup/index.tsx
+++ igniteTeams/src/screens/Groups/NewGroup/index.tsx
@@ -11,7 +11,7 @@ export function NewGroup (){
             <Content>
                 <Icon />
                 <Highlight title='Nova turma' subtitle='Crie a turma para adicionar as pessoas ' />
-                <Input />
+                <Input placeholder='Nome da Turma' />
                 <Button title='Criar'  style={{marginTop:20}}/>
             </Content>
         </Container>
