27d5191e49fa72f47aa513e6b16f52aee7b055af
saulo1212 <saulodetacio715@gmail.com>
2024-09-14 13:46:37 -0300

    prevent glitch

--- igniteTeams/src/screens/Groups/NewGroup/index.tsx
+++ igniteTeams/src/screens/Groups/NewGroup/index.tsx
@@ -4,14 +4,17 @@ import { Highlight } from '@components/Highlight'
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
 
 export function NewGroup (){
+
+    const [group,setGroup] = useState('')
 
     const navigation = useNavigation()
 
     function handleNew(){
         navigation.navigate('players', {
-            group:'Rocket'
+            group
         })
     }
 
@@ -21,7 +24,10 @@ export function NewGroup (){
             <Content>
                 <Icon />
                 <Highlight title='Nova turma' subtitle='Crie a turma para adicionar as pessoas ' />
-                <Input placeholder='Nome da Turma' />
+                <Input 
+                    placeholder='Nome da Turma' 
+                    onChangeText={setGroup}
+                />
                 <Button 
                     title='Criar'  
                     style={{marginTop:20}}
