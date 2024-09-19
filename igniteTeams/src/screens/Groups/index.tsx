ee3bf2a77863b1951b4277f8c568eb808bafb950
saulo1212 <saulodetacio715@gmail.com>
2024-09-19 08:09:40 -0300

    list players

--- igniteTeams/src/screens/Groups/index.tsx
+++ igniteTeams/src/screens/Groups/index.tsx
@@ -29,6 +29,12 @@ export default function Groups() {
     }
   }
 
+  function handleOpenGroup(group:string){
+    navigation.navigate('players', {
+      group
+    })
+  }
+
   useFocusEffect(useCallback(() => {
     fetchGroups()
   },[]))
@@ -47,7 +53,10 @@ export default function Groups() {
         data={groups}
         keyExtractor={item => item}
         renderItem={({item}) => (
-          <GroupCard title={item}/>
+          <GroupCard 
+            title={item}
+            onPress={() => handleOpenGroup(item)}
+          />
         )}
 
         contentContainerStyle={groups.length === 0 && {flex:1}}
--- igniteTeams/src/screens/Players/index.tsx
+++ igniteTeams/src/screens/Players/index.tsx
@@ -9,12 +9,15 @@ import {
     HeaderList, 
     NumbersOffPlayers
 } from './styles'
-import { FlatList } from 'react-native'
+import { Alert, FlatList } from 'react-native'
 import { useState } from 'react'
 import { PlayerCard } from '@components/PlayerCard'
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
 import { useRoute } from '@react-navigation/native'
+import { AppError } from '@utils/appError'
+import { playerAddByGroup } from '@storage/player/playerAddByGroup'
+import { playersGetByGroup } from '@storage/player/playersGetByGroup'
 
 type RouteParams = {
     group: string
@@ -22,12 +25,43 @@ type RouteParams = {
 
 export function Players(){
 
+    const [newPlayerName, setNewPlayerName] = useState('')
     const [team, setTeam] = useState('TIME A')
     const [players, setPlayers] = useState([])
 
     const route = useRoute()
 
     const {group} = route.params as RouteParams
+
+    async function handleAddPlayer() {
+
+        if(newPlayerName.trim().length === 0){
+            return Alert.alert('Nova pessoa', 'Informe o nome da pessoa')
+        }
+
+        const newPlayer = {
+            name: newPlayerName,
+            team
+        }
+
+        try {
+
+            await playerAddByGroup(newPlayer, group)
+
+            const players = await playersGetByGroup(group)
+            console.log(players)
+            
+        } catch (error) {
+            if(error instanceof AppError){
+                Alert.alert('Nova pessoa', error.message)
+            }else{
+                console.log(error)  
+                Alert.alert('Nova pessoa', 'Não foi possivel adicionar')
+                
+            }
+        }
+
+    }
 
     return(
         <Container>
@@ -40,11 +74,15 @@ export function Players(){
 
             <Form>
                 <Input 
+                    onChangeText={setNewPlayerName}
                     placeholder='Nome da pessoa'
                     autoCorrect={false}
                 />
 
-                <ButtonIcon icon='add' />
+                <ButtonIcon 
+                    icon='add' 
+                    onPress={() => handleAddPlayer()}
+                />
 
             </Form>
 
